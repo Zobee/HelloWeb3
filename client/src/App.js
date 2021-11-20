b05f824dac32380fd12d602b65de0e5c3c375421
Zoheb Boga <zohebboga@hotmail.com>
2021-11-20 00:17:52 -0500

    Implement styling for client-side

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,69 @@
-import * as React from "react";
+import React, {useState, useEffect} from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from './utils/contractABI.json'
 
 export default function App() {
 
-  const wave = () => {
+  const contractAddress = "0x99c65355CdA66FC9b036C0C16c3A8debA795F66F";
+  const contractABI = abi.abi;
+  
+  const [currAccount, setCurrAccount] = useState(null);
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) return;
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      if (accounts.length !== 0) {
+        setCurrAccount(accounts[0])
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch(err) {
+      console.log(err);
+    }
     
+  }
+  
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+  
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+  
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected", accounts[0]);
+      setCurrAccount(accounts[0]); 
+  
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  })
+  
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+  
+        let count = await wavePortalContract.getTotalHellos();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
   
   return (
@@ -13,16 +71,27 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+        👋 What's up Metaverse?
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I'm Zoheb, a full-stack developer who is passionate about finding solutions to problems! You got a problem? I'm here to solve it!
         </div>
 
+        {currAccount ? 
         <button className="waveButton" onClick={wave}>
-          Wave at Me
+          Say Hello!
         </button>
+        :
+        <button className="waveButton" onClick={connectWallet}>
+          Connect Your Wallet!
+        </button>
+        }
+
+        <div className="newestHelloContainer">
+          <h1>Most recent hello</h1>
+          <p>No hello's yet :(</p>
+        </div>
       </div>
     </div>
   );
